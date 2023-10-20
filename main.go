527da68cc08afa94c0bfffbbd98ce047c1e3c4b2
Roch <rochishnu7@gmail.com>
2023-10-21 01:57:47 +0530

    Impl view edit and save functionality

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
 	"os"
 	"net/http"
+	"html/template"
 )
 
 
@@ -32,11 +33,49 @@ func loadPage(title string) (*Page , error){
 func viewHandler( w http.ResponseWriter , r *http.Request ){
 
 	title := r.URL.Path[len("/view/"):]
+	// fmt.Println(len("/view/"))  -> 6
+	
+	p , err := loadPage(title)
 
-	p , _ := loadPage(title)
+	if err != nil {
+		http.Redirect(w , r , "/edit/"+title , http.StatusNotFound )
+	}
+
+	t , _ := template.ParseFiles("view.html")
+
+	t.Execute(w,p)
 
 
-	fmt.Fprintf( w, " <h1>%s</h1>  <h5>%s</h5>" , p.Title , p.Body)
+}
+
+
+func editHandler( w http.ResponseWriter , r *http.Request ){
+
+	title := r.URL.Path[ len("/view/") : ]
+
+	p , err := loadPage(title)
+
+	if err != nil{
+		p = &Page{ Title: title  }
+	}
+
+	t , _ := template.ParseFiles("edit.html")
+	t.Execute(w , p)
+
+}
+
+
+func saveHandler( w http.ResponseWriter , r *http.Request ){
+
+	title := r.URL.Path[len("/save/") : ]
+
+	body := r.FormValue("body")
+
+	p := &Page{ Title: title , Body: []byte(body) }
+	p.save()
+
+	http.Redirect(w,r , "/view/"+title , http.StatusNotFound )
+
 }
 
 
@@ -55,6 +94,8 @@ func main() {
 
 
 	http.HandleFunc("/view/" , viewHandler)
+	http.HandleFunc("/edit/" , editHandler)
+	http.HandleFunc("/save/" , saveHandler)
 	log.Fatal(http.ListenAndServe(":8080" , nil))
 
 	
